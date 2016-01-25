df4b7ab3a218e43d91df2952941f2e4afc85ccca
SupportVectorMcs <ronhe44@gmail.com>
2016-01-25 16:46:52 -0500

    randomizedMotifSearch

--- 1_Finding_Hidden_Messages_in_DNA/week_3/randomizedMotifSearch.py
+++ 1_Finding_Hidden_Messages_in_DNA/week_3/randomizedMotifSearch.py
@@ -1,36 +1,41 @@
-def greedyMotifSearchwithPseudocounts(k, t, input):
-    dnas = input.split(',')
-    bestMotifs = []
-    for i in range(0, len(dnas)):
-        bestMotifs.append(dnas[i][0:0+k])
-    for i in range(0, len(dnas[0]) - k + 1):
-        motifs = []
-        motifs.append(dnas[0][i:i+k])
-        for j in range(1, t):
-            profile = generateProfile(motifs)
-            kmer = profilemostProbableKmer(dnas[j], k, profile)
-            motifs.append(kmer)
-        if getScore(motifs) < getScore(bestMotifs):
-            bestMotifs = motifs
-    print bestMotifs
+import random, sys
+
+def randomizedMotifSearch(input, k, t):
+    random.seed()
+    minScore = sys.maxint
+    motifs = []
+    for i in range(0, 1000):
+        curMotifs = randomizedMotifSearch_(input, k, t)
+        curScore = getScore(curMotifs)
+        if curScore < minScore:
+            minScore = curScore
+            motifs = curMotifs
+    for motif in motifs:
+        print motif
+    print minScore
     return
 
-def getScore(motifs):
-    score = 0
-    for i in range(0, len(motifs[0])):
-        stat = {'A':0, 'C':0, 'G':0, 'T':0}
-        for motif in motifs:
-            stat[motif[i]] = stat[motif[i]] + 1
-        maxVal = 0
-        for k, v in stat.items():
-            if v > maxVal:
-                maxVal = v
-        score = score + len(motifs) - maxVal 
-    return score
+def randomizedMotifSearch_(input, k, t):
+    dnas = input.split()
+    motifs = []
+    bestMotifs = []
+    for i in range(0, t):
+        rand = random.randint(0, len(dnas[0]) - k)
+        motifs.append(dnas[i][rand:rand+k])
+    bestMotifs = motifs
+    while True:
+        profile = generateProfile(motifs)
+        motifs = []
+        for j in range(0, len(dnas)):
+            motifs.append(profilemostProbableKmer(dnas[j], k, profile))
+        if getScore(motifs) < getScore(bestMotifs):
+            bestMotifs = motifs
+        else:
+            return bestMotifs
 
 def profilemostProbableKmer(dna, k, matrix):
     maxProb = 0
-    result = dna[0:0+k]
+    result = ''
     for i in range(0, len(dna) - k + 1):
         kmer = dna[i:i+k]
         curProb = 1
@@ -47,6 +52,19 @@ def profilemostProbableKmer(dna, k, matrix):
             maxProb = curProb
             result = kmer
     return result
+
+def getScore(motifs):
+    score = 0
+    for i in range(0, len(motifs[0])):
+        stat = {'A':0, 'C':0, 'G':0, 'T':0}
+        for motif in motifs:
+            stat[motif[i]] = stat[motif[i]] + 1
+        maxVal = 0
+        for k, v in stat.items():
+            if v > maxVal:
+                maxVal = v
+        score = score + len(motifs) - maxVal 
+    return score
 
 def generateProfile(dnas):
     matrix = []
